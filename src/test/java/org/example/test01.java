5d88d70b7a659a787563345f85feff92786a5f47
xiedixi <xiedixi@gmail.com>
2022-07-12 18:56:42 +0800

    git修改

--- src/test/java/org/example/test01.java
+++ src/test/java/org/example/test01.java
@@ -3,5 +3,7 @@ package org.example;
 public class test01 {
     public static void main(String[] args) {
         System.out.printf("Hello Git");
+        System.out.printf("Hello git02");
+        System.out.println("Hello git03");
     }
 }
